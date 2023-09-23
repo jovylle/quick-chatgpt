07dded5264dd85dd4d8acec24d06c30fd5876e59
Jovylle Bermudez CMD <jovyllebermude.z@gmail.com>
2023-09-23 16:16:14 +0800

    add front-end to the serverless function

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -1,35 +1,36 @@
-// Import necessary libraries
-const { v4: uuidv4 } = require('uuid');
 const OpenAI = require('openai');
 
-// Configure your OpenAI API key
 const openai = new OpenAI({
   apiKey: process.env.MY_OPENAI_API,
 });
 
-exports.handler = async (event, context) => {
-  // console.log("openai.completions")
-  // console.log(openai.completions)
-  try {
-    const requestBody = JSON.parse(event.body);
-    const { message } = requestBody;
-    // You can customize this part to make API calls to OpenAI's GPT-3 model
-    const response = await openai.completions.create({
-      model: 'text-davinci-003',       // use 'text-davinci-002' for GPT-2 
-      prompt: message,
-      max_tokens: 50,
-    });
-    // console.log(response.choices)
-    const chatResponse = response.choices[0].text;
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: chatResponse }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
+exports.handler = async function (event, context) {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
   }
+
+  const { message } = JSON.parse(event.body);
+
+  const gptResponse = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-16k',
+    max_tokens: 256,
+    messages: [
+      {
+        role: 'system',
+        content: 'You are a helpful assistant.'
+      },
+      {
+        role: 'user',
+        content: message
+      },
+    ]
+  });
+
+  console.log("hohoo")
+  console.log(gptResponse.choices[0].message)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: gptResponse.choices[0].message.content })
+  };
 };
